a13f7404166167801696e4a85299646f38ab4b27
Jake Mason <dev.jakemason@gmail.com>
2020-01-31 14:01:17 -0500

    Rodents of regular size wave

--- Assets/Scripts/Models/Cards/Deck.cs
+++ Assets/Scripts/Models/Cards/Deck.cs
@@ -13,7 +13,6 @@ public class Deck : ScriptableObject
     {
         // this makes the editor typing experience feel a little weird, but saves us from having to manually
         // rename the file to match the name of the card. I feel this is useful enough to keep for now.
-        //TODO: This is suddenly spamming really annoying console messages so I've disabled it for now.
         if (Name != "")
         {
             string assetPath = AssetDatabase.GetAssetPath(GetInstanceID());
--- Assets/Scripts/Models/Enemy.cs
+++ Assets/Scripts/Models/Enemy.cs
@@ -11,4 +11,18 @@ public class Enemy : ScriptableObject
     [Range(0.5f, 1.5f)] public float MaxSize = 1.0f;
     public Sprite Sprite;
     public int Damage;
+
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        // this makes the editor typing experience feel a little weird, but saves us from having to manually
+        // rename the file to match the name of the card. I feel this is useful enough to keep for now.
+        if (Name != "")
+        {
+            string assetPath = AssetDatabase.GetAssetPath(GetInstanceID());
+            AssetDatabase.RenameAsset(assetPath, Name);
+        }
+    }
+#endif
 }
