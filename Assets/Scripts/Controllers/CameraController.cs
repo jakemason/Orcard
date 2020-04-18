e02755fcdf45bec272755f8c633702136fda53a0
Jake Mason <dev.jakemason@gmail.com>
2020-04-18 14:41:56 -0400

    Remove old DawnLike tiles, add Camera UI move prompt

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
     public float Speed = 12f;
     public Vector2 Bounds;
+    private bool _hasMoved = false;
+    [FormerlySerializedAs("UITarget")] public FadeOverTime FadeTarget;
 
     private void Update()
     {
         float horizontalMovement = Input.GetAxis("Horizontal");
         if (horizontalMovement != 0.0f)
         {
+            if (!_hasMoved)
+            {
+                _hasMoved = true;
+                FadeTarget.Activate();
+            }
+
             Vector3 pos = transform.position;
             pos.x              += horizontalMovement * Speed * Time.deltaTime;
             pos.x              =  Mathf.Clamp(pos.x, Bounds.x, Bounds.y);
---
+++ Assets/Scripts/Views/FadeOverTime.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+public class FadeOverTime : MonoBehaviour
+{
+    public float FadeSpeed = 0.5f;
+    private SpriteRenderer _renderer;
+    private Color _colorTracker;
+    private bool _isActive = false;
+
+    private void Start()
+    {
+        _renderer     = GetComponent<SpriteRenderer>();
+        _colorTracker = _renderer.color;
+    }
+
+    public void Activate()
+    {
+        _isActive = true;
+    }
+
+    private void Update()
+    {
+        if (!_isActive) return;
+
+        _colorTracker = new Color(_colorTracker.r, _colorTracker.g, _colorTracker.b,
+            _colorTracker.a - (FadeSpeed * Time.deltaTime));
+        _renderer.color = _colorTracker;
+
+        if (_colorTracker.a <= 0.0f)
+        {
+            Destroy(this);
+        }
+    }
+}
