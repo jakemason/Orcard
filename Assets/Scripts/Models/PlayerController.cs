c55a4c9ced4df46a03b5262cc1df9c2839e5a0d1
Jake Mason <dev.jakemason@gmail.com>
2020-04-20 21:26:16 -0400

    Move sprites around to appease TextMeshPro. Add icons sprite sheet for testing.

--- Assets/Scripts/Models/PlayerController.cs
+++ Assets/Scripts/Models/PlayerController.cs
@@ -99,10 +99,11 @@ namespace Players
 
         private void UpdateDrawTimer()
         {
-            _freeDrawCooldown       -= Time.deltaTime;
-            DrawCooldownText.text   =  _freeDrawCooldown <= 0f ? "!" : _freeDrawCooldown.ToString("00");
-            DrawButton.interactable =  _freeDrawCooldown <= 0f || IncomeController.GetCurrentGold() >= RedrawGoldCost;
-            RedrawCostText.text     =  RedrawGoldCost.ToString();
+            _freeDrawCooldown -= Time.deltaTime;
+            DrawCooldownText.text =
+                _freeDrawCooldown <= 0f ? "<sprite=\"Icons\" index=0>" : _freeDrawCooldown.ToString("00");
+            DrawButton.interactable = _freeDrawCooldown <= 0f || IncomeController.GetCurrentGold() >= RedrawGoldCost;
+            RedrawCostText.text     = RedrawGoldCost.ToString();
             RedrawCostIndicator.SetActive(_freeDrawCooldown >= 0.0f);
         }
 
