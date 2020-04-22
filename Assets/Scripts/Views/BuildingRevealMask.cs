3a56cc153ae52ec27b6a166121916c0c24deb0ee
Jake Mason <dev.jakemason@gmail.com>
2020-04-22 00:07:22 -0400

    Update build reveal mask image

---
+++ Assets/Scripts/Views/BuildingRevealMask.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BuildingRevealMask : MonoBehaviour
+{
+    public float RevealSpeed = 1.0f;
+
+    private void Update()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.y         += RevealSpeed * Time.deltaTime;
+        transform.localScale =  localScale;
+        if (localScale.y >= 8.0f)
+        {
+            Destroy(this);
+        }
+    }
+}
