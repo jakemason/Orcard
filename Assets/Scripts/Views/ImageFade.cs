3fc4ee758ff0aeba82955f28d7dc4473ade40f8d
Jake Mason <dev.jakemason@gmail.com>
2020-10-04 18:29:39 -0400

    Add Studio Logo intro Scene.
    Idea -- have the logo "pixelate" itself via shader after it loads in to
    better match the aesthetic of the main menu? A kind of "now we're going
    old-school" transition.

--- Assets/Scripts/Views/ImageFade.cs
+++ Assets/Scripts/Views/ImageFade.cs
@@ -12,8 +12,13 @@ public class ImageFade : MonoBehaviour
     private void Start()
     {
         _image = GetComponent<Image>();
+
         Color color = _image.color;
-        color.a      = 1;
+        if (FadeSpeed >= 0.0f)
+        {
+            color.a = 1.0f;
+        }
+
         _image.color = color;
     }
 
@@ -22,13 +27,29 @@ public class ImageFade : MonoBehaviour
         Delay -= Time.deltaTime;
         if (Delay <= 0.0f)
         {
-            Color color = _image.color;
-            color = new Color(color.r, color.g, color.b,
-                color.a - FadeSpeed * Time.deltaTime);
-            _image.color = color;
-            if (color.a <= 0.0f)
+            if (FadeSpeed >= 0.0f)
             {
-                Destroy(gameObject);
+                Color color = _image.color;
+                color = new Color(color.r, color.g, color.b,
+                    color.a - (FadeSpeed * Time.deltaTime));
+
+                _image.color = color;
+                if (color.a <= 0.0f)
+                {
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                Color color = _image.color;
+                color = new Color(color.r, color.g, color.b,
+                    color.a - (FadeSpeed * Time.deltaTime));
+                if (color.a > 1.0f)
+                {
+                    color.a = 1.0f;
+                }
+
+                _image.color = color;
             }
         }
     }
